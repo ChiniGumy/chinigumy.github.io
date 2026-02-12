b4d41a27014b2f36128af137b5dbce2954d5ce55
David Sanchez <dsebastiansr24@gmail.com>
2026-02-12 18:09:10 -0500

    [FIX] url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://dsebastiansr.github.io',
-  base: '/dsebastiansr.github.io'
+  site: "https://dsebastiansr.github.io",
+  base: "/",
 });
